f98c440974c67c1476f27ee591563a9551f44c9a
affian <nnamdiaffia@gmail.com>
2021-10-29 14:03:25 +0100

    Delete seed_airbnb.sh

--- seed_airbnb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mongoimport --authenticationDatabase admin \
-  --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD \
-  --db $MONGO_DB_NAME \
-  --collection $MONGO_COLLECTION_NAME \
-  --type $MONGO_DB_TYPE \
-  --file /tmp/chicago_listings.csv \
-  --headerline
